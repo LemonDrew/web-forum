37b8a073649fe7706348fa07ebc87850051248f1
Derrick Wong <142132416+LemonDrew@users.noreply.github.com>
2025-01-02 14:21:44 +0800

    Add function to insert data to the database

--- backend/internal/dataaccess/users.go
+++ backend/internal/dataaccess/users.go
@@ -1,6 +1,7 @@
 package users
 
 import (
+	"log"
 	"github.com/CVWO/sample-go-app/internal/database"
 	"github.com/CVWO/sample-go-app/internal/models"
 )
@@ -13,4 +14,16 @@ func List(db *database.Database) ([]models.User, error) {
 		},
 	}
 	return users, nil
+}
+
+func RegisterUser(name string) error {
+	query := `INSERT INTO "Users" (name) VALUES ($1)`
+	_, err := database.DB.Exec(query, name)
+	if err != nil {
+		log.Printf("Failed to insert user: %v", err)
+		return err
+	}
+
+	log.Printf("User '%s' added successfully!", name)
+	return nil
 }
